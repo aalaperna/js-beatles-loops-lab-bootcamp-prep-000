df8cb16244905153d063416e02b38f75345adabd
Alyssa LaPerna <aalaperna@gmail.com>
2018-02-08 03:41:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function johnLennonFacts(array) {
 let i = 0
 const newArray = []
 while (i < array.length) {
-  newArray.push('${array[i] + "!!!"}')
+  newArray.push(array[i] + "!!!")
   i++
   }
   return newArray
